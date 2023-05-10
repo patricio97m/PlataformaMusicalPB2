972e4139002a4ba1e0e9c58a45fb6c6a49a954a8
patricio97m <patricio.97m@gmail.com>
2023-05-10 09:28:49 -0300

    Primer Commit

---
+++ test/plataformaMusical/PruebaPlataformaMusical.java
@@ -0,0 +1,14 @@
+package plataformaMusical;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PruebaPlataformaMusical {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
