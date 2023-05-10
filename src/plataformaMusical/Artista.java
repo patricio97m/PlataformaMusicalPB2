3309606d0e1399c49a95431b097f11ac22223815
patricio97m <patricio.97m@gmail.com>
2023-05-10 16:35:55 -0300

    Implementacion de Usuario y Artista

--- src/plataformaMusical/Artista.java
+++ src/plataformaMusical/Artista.java
@@ -1,5 +1,28 @@
 package plataformaMusical;
 
 public class Artista {
+	private String nombre;
+	private String genero;
 
+	public Artista(String nombre, String genero) {
+	  this.nombre = nombre;
+	  this.genero = genero;
+	}
+
+	public String getNombre() {
+	  return nombre;
+	}
+
+	public String getGenero() {
+	  return genero;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+	  if (obj instanceof Artista) {
+	    Artista other = (Artista) obj;
+	    return nombre.equals(other.nombre) && genero.equals(other.genero);
+	  }
+	  return false;
+	}
 }
--- src/plataformaMusical/Usuario.java
+++ src/plataformaMusical/Usuario.java
@@ -1,5 +1,19 @@
 package plataformaMusical;
 
 public class Usuario {
+	private String usuario;
+	private String contraseña;
 
+	public Usuario(String usuario, String contraseña) {
+	  this.usuario 		= usuario;
+	  this.contraseña 	= contraseña;
+	}
+
+	public String getUsuario() {
+	  return usuario;
+	}
+
+	public boolean checkearContraseña(String contraseña) {
+	  return this.contraseña.equals(contraseña);
+	}
 }
